f1a20ae6f6952d29d2635788a636ee840cb186cd
TimothyFothergill <timothy.fothergill@digital.hmrc.gov.uk>
2022-02-27 22:52:05 +0000

    CVG-1018 adds Balloons Away to project list.

--- app/com/timlah/services/CurrentProjects.scala
+++ app/com/timlah/services/CurrentProjects.scala
@@ -9,6 +9,11 @@ class CurrentProjects {
   def currentProjects(): Seq[Project] = {
     Seq(
       Project(
+        title       = "Balloons Away",
+        description = "A game inspired by Helicopter game. Made in a day. Some flickering due to the speed of the obstacles. All assets and code by me, except the music track. Hold down the Left Mouse Button (LMB) to rise. Release LMB to fall. NOTE: You'll need to unmute the audio on the Unity Play WebGL player.",
+        url         = "https://play.unity.com/mg/other/v0-1-ih5y"
+      ),
+      Project(
         title       = "Matchagana",
         description = "Match Hiragana to the Romaji equivalent - This was my first personal project in Python using the Flask framework. NOTE: This is hosted on a free Heroku plan. It can take a while to load initially.",
         url         = "http://matchagana.herokuapp.com"
