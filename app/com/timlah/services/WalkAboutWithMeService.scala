38fb3a843b3cb34d62c69190e1b9617fd8744336
TimothyFothergill <67912934+TimothyFothergill@users.noreply.github.com>
2022-10-24 19:57:41 +0100

    CVG-1058 (#25)
    * CVG-1058 amend cached duration

--- app/com/timlah/services/WalkAboutWithMeService.scala
+++ app/com/timlah/services/WalkAboutWithMeService.scala
@@ -12,7 +12,7 @@ class WalkAboutWithMeService {
   def downloadImage(imageURL: Seq[String], imageDate: Seq[String], path: String): Unit = {
    imageURL.foreach(image => {
      imageDate.foreach(date => {
-      new URL(image) #> new File("public/Walk " + date.replace("00:00:00 GMT","") + ".png") !!
+      new URL(image) #> new File("public/images/walk-images/Walk " + date.replace("00:00:00 GMT","") + ".png") !!
      })
    })
   }
