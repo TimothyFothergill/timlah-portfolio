854d70cc78ba2eb540b711e8a7bea59d1433d3a8
TimothyFothergill <timothy.fothergill@digital.hmrc.gov.uk>
2022-10-27 20:15:59 +0100

    CVG-1061-update-walk-img update location

--- app/com/timlah/connectors/WalkAboutWithMeConnector.scala
+++ app/com/timlah/connectors/WalkAboutWithMeConnector.scala
@@ -10,7 +10,8 @@ import scala.concurrent.{ExecutionContext, Future}
 @Singleton
 class WalkAboutWithMeConnector @Inject()(implicit val ec: ExecutionContext) {
   def getAllWalkAboutWithMeData: Future[Seq[WalkAboutWithMe]] = {
-    val r: Response = requests.get("http://walk-about-with-me-api.herokuapp.com/?marker1lat=45.395738&marker1long=141.702843&marker2lat=44.812031&marker2long=142.074650")
+//    requests.get("http://walk-about-with-me-api.herokuapp.com/?marker1lat=45.395738&marker1long=141.702843&marker2lat=44.812031&marker2long=142.074650")
+    val r: Response = requests.get("http://walk-about-with-me-api.herokuapp.com/?marker1lat=44.812031&marker1long=142.074650&marker2lat=44.34943456506206&marker2long=142.46409953518858")
     Future(Json.parse(r.text).as[Seq[WalkAboutWithMe]])
   }
 }
