f7e0f3fbd521bb25738ce668eec1b7f95ac81221
TimothyFothergill <67912934+TimothyFothergill@users.noreply.github.com>
2022-11-30 22:19:31 +0000

    CVG-1069 Update walk about positions (#32)
    * CVG-1069 update positions

--- app/com/timlah/connectors/WalkAboutWithMeConnector.scala
+++ app/com/timlah/connectors/WalkAboutWithMeConnector.scala
@@ -10,7 +10,7 @@ import scala.concurrent.{ExecutionContext, Future}
 @Singleton
 class WalkAboutWithMeConnector @Inject()(implicit val ec: ExecutionContext) {
   def getAllWalkAboutWithMeData: Future[Seq[WalkAboutWithMe]] = {
-    val r: Response = requests.get("http://walk-about-with-me-api.herokuapp.com/?marker1lat=43.331180951043265&marker1long=141.861271595367&marker2lat=43.0556336617387&marker2long=141.35340782203917")
+    val r: Response = requests.get("http://walk-about-with-me-api.herokuapp.com/?marker1lat=43.0556336617387&marker1long=141.35340782203917&marker2lat=42.45221975618502&marker2long=141.1802584555724")
     Future(Json.parse(r.text).as[Seq[WalkAboutWithMe]])
   }
 }
