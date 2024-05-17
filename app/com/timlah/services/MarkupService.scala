077bed0226ef9c126b9dbd223015ffe457599305
TimothyFothergill <67912934+TimothyFothergill@users.noreply.github.com>
2024-05-17 14:09:10 +0100

    CVG-1104 fix compile warnings

--- app/com/timlah/services/MarkupService.scala
+++ app/com/timlah/services/MarkupService.scala
@@ -15,7 +15,7 @@ class MarkupService {
   def markdownFileToHTML(): Either[String, String] = {
     val file = scala.io.Source.fromResource("latest-blog.md")
     val lines =
-      try file.getLines.toList.filterNot(_.isEmpty)
+      try file.getLines().toList.filterNot(_.isEmpty)
       finally file.close()
     transformer.transform(lines.take(100).mkString("\n")) match {
       case Left(_)  => Left("<Unable to render the post at this time.>")
