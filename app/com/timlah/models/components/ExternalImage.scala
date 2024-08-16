c0a503b97c6581be63702f5faa7b0baa72468ebf
Tim Fothergill <timothy.fothergill@gmail.com>
2024-08-16 21:56:30 +0100

    CVG-1114 add Kitacon gallery

--- app/com/timlah/models/components/ExternalImage.scala
+++ app/com/timlah/models/components/ExternalImage.scala
@@ -3,5 +3,7 @@ package com.timlah.models.components
 case class ExternalImage(
     alt: Option[String],
     title: String,
-    url: String
+    url: String,
+    linkContent: Option[String] = None,
+    landscape: Boolean = true
 )
